990657bc8230efd22a98bbe0c81b53d4d5ae3ae4
Forrest Galloway <forrest.galloway@mosaik.com>
2015-09-26 11:04:35 -0500

    Add actions constants. Add keyMirror package, it was removed from React 0.14

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './components/App.jsx'
+import action from './constants/constants.js';
 import store from './store/createStore.js';
 
 
