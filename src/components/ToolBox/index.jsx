f4fb3e1ca72a113146b1498cc10e02ab17ae84ab
Forrest Galloway <forrest.galloway@mosaik.com>
2015-09-28 20:29:48 -0500

    Add .editorconfig, placeholders folders/files for UI components.

---
+++ src/components/ToolBox/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class ToolBox extends Component {
+
+    static propTypes = {
+        
+    };
+
+    render() {
+        const { children } = this.props;
+        return (
+            <div>
+              <h1>ToolBox</h1>
+            </div>
+        );
+    }
+}
---
+++ src/components/WorkBench/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class WorkBench extends Component {
+
+    static propTypes = {
+        
+    };
+
+    render() {
+        const { children } = this.props;
+        return (
+            <div>
+              <h1>WorkBench</h1>
+            </div>
+        );
+    }
+}
